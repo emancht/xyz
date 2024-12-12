5b8248e92ea4dd5e0b57a6188701e683717cd03e
emancht <emancht@gmail.com>
2024-12-12 21:57:52 +0600

    gl

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      "/api/":{
-        target: "https://xyz-nu-six.vercel.app/"
-        // target: "http://localhost:3003"
-      }
+   proxy:{
+    "/api/":{
+      target: "https://xyz-nu-six.vercel.app"
+      // target: "http://localhost:3003"
     }
   }
 })
